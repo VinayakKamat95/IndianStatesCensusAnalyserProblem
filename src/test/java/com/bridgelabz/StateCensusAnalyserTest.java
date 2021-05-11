f08954f35df414272d03648f8792d7333602ca2e
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-05-11 15:40:21 +0530

    Analyser to load the Indian States Census Information from a csv file

---
+++ src/test/java/com/bridgelabz/StateCensusAnalyserTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.IOException;
+
+public class StateCensusAnalyserTest {
+    public static final String STATECENSUS_CSVFILE = "./src/main/resources/StateCensusData.csv";
+    public static final String STATECODES_CSVFILE = "./src/main/resources/IndiaStateCode.csv";
+    public static final String WRONG_FILE = "/useless.txt";
+
+    @Test
+    public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+        try {
+            int count = stateCensusAnalyser.loadCsvData(STATECENSUS_CSVFILE, StateCensus.class);
+            System.out.println(count);
+            Assert.assertEquals(29, count);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
